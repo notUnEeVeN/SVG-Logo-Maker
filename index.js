1bf8172f8906e5f36862e975b641ef837450330f
notUnEeVeN <tybalt.mallet@gmail.com>
2023-08-20 14:03:53 -0700

    completed functionality/design for all 3 shapes, first iteration of index.js and shapes.js files is complete

--- index.js
+++ index.js
@@ -1,46 +1,75 @@
-const inquirer = require('inquirer');
-const { Circle, Triangle, Square } = require('./lib/shapes');
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+//importing the cirlce triangle and square svg code from our shapes.js
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 function main() {
-    const questions = [
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Enter up to three characters for logo text:',
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter text color (keyword or hexadecimal):'
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape:',
-            choices: ['circle', 'triangle', 'square']
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter shape color (keyword or hexadecimal):'
-        }
-    ];
+  const questions = [
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to three characters for logo text:",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter text color (keyword or hexadecimal):",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape:",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Enter shape color (keyword or hexadecimal):",
+    },
+  ];
 
-    inquirer.prompt(questions).then(answers => {
-        let shapeObj;
-        switch (answers.shape) {
-            case 'circle':
-                shapeObj = new Circle(answers.shapeColor);
-                break;
-            case 'triangle':
-                shapeObj = new Triangle(answers.shapeColor);
-                break;
-            case 'square':
-                shapeObj = new Square(answers.shapeColor);
-                break;
-        }
-    }).catch(error => {
-        console.error("An error occurred:", error);
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      let shapeObj;
+      switch (answers.shape) {
+        case "circle":
+          shapeObj = new Circle(answers.shapeColor);
+          break;
+        case "triangle":
+          shapeObj = new Triangle(answers.shapeColor);
+          break;
+        case "square":
+          shapeObj = new Square(answers.shapeColor);
+          break;
+      }
+      // declaring a default fontsize and position that changes on if the selected shape is a triangle, because if I have a default size
+      //the letters overlap and stick out of the triangle
+      let fontSize = 60;
+      let textYPosition = 125;
+
+      if (answers.shape === "triangle") {
+        fontSize = 45;
+        textYPosition = 140;
+      }
+
+      const svgContent = `
+<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  ${shapeObj.render()}
+  <text x="150" y="${textYPosition}" font-size="${fontSize}" text-anchor="middle" fill="${
+        answers.textColor
+      }">
+    ${answers.text}
+  </text>
+</svg>
+`;
+
+      fs.writeFileSync("logo.svg", svgContent);
+      console.log("Generated logo.svg");
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
     });
 }
+
+main();
